5cbbc1b173cc22f772ea3fccf688c98311c51633
Thomas William Komp <twkomp@gmail.com>
2018-06-13 15:46:53 -0400

    added gui

--- app/src/main/java/com/example/thomaskomp/lifecycleexample/MainActivity.java
+++ app/src/main/java/com/example/thomaskomp/lifecycleexample/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.thomaskomp.lifecycleexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Intent newActivity;
+    private Intent newActivityButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+        newActivity = new Intent(this,SecondActivity.class);
+    }
+
+    private void setListeners() {
+        //newActivityButton
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
     }
 }
