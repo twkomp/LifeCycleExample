735096f67923e7a05dffd530df0c2c4cf005fc1d
Thomas William Komp <twkomp@gmail.com>
2018-06-14 11:01:36 -0400

    added buttons

--- app/src/main/java/com/example/thomaskomp/lifecycleexample/MainActivity.java
+++ app/src/main/java/com/example/thomaskomp/lifecycleexample/MainActivity.java
@@ -3,21 +3,40 @@ package com.example.thomaskomp.lifecycleexample;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Intent newActivity;
-    private Intent newActivityButton;
+    private Button newActivityButton;
+    private Button finishProgramButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
         newActivity = new Intent(this,SecondActivity.class);
+        newActivityButton = findViewById(R.id.new_activity_button);
+        finishProgramButton = findViewById(R.id.finish_activity_button);
+        // = new Intent(this,SecondActivity.class);
+        setListeners();
     }
 
     private void setListeners() {
-        //newActivityButton
+        newActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(newActivity);
+            }
+        });
+
+        finishProgramButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/thomaskomp/lifecycleexample/SecondActivity.java
+++ app/src/main/java/com/example/thomaskomp/lifecycleexample/SecondActivity.java
@@ -2,12 +2,26 @@ package com.example.thomaskomp.lifecycleexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
-
+    private Button finishButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        finishButton = findViewById(R.id.finish_button);
+        setListeners();
+    }
+
+    private void setListeners() {
+    finishButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
+    });
     }
 }
